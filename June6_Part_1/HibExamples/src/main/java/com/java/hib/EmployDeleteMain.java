1359e817f315ad10af1b9b66f34e645df2a81def
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-06-06 10:47:05 +0530

    code

---
+++ June6_Part_1/HibExamples/src/main/java/com/java/hib/EmployDeleteMain.java
@@ -0,0 +1,25 @@
+package com.java.hib;
+
+import java.util.Scanner;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class EmployDeleteMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Employ Number  ");
+		int empno = sc.nextInt();
+		SessionFactory sf = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		Query query = session.createQuery("from Employ where empno = " + empno);
+		Employ employ =(Employ)query.uniqueResult();
+		Transaction trans = session.beginTransaction();
+		session.delete(employ);
+		trans.commit();
+		System.out.println("*** Employ Record Deleted ***");
+	}
+}
---
+++ June6_Part_1/HibExamples/src/main/java/com/java/hib/EmploySearchMain.java
@@ -0,0 +1,23 @@
+package com.java.hib;
+
+import java.util.Scanner;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class EmploySearchMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Employ Number  ");
+		int empno = sc.nextInt();
+		SessionFactory sf = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		Query query = session.createQuery("from Employ where empno = " + empno);
+		Employ employ =(Employ)query.uniqueResult();
+		if (employ !=null) {
+			System.out.println(employ);
+		}
+	}
+}
