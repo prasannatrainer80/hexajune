6ed1a3166545c56dee447dbc7f989d9b6dfd0a7c
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-06-19 11:57:29 +0530

    code

---
+++ June_19_Part_2/SbSecurityNewEx1/src/main/java/com/example/demo/SbSecurityNewEx1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityNewEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityNewEx1Application.class, args);
+	}
+
+}
---
+++ June_19_Part_2/SbSecurityNewEx2/src/main/java/com/example/demo/SbSecurityNewEx2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityNewEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityNewEx2Application.class, args);
+	}
+
+}
