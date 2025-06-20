a32c19da6b9ec0c16781158b2faa2cd345309c4f
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-06-20 10:30:00 +0530

    code

---
+++ June_20/EurekaDbClientNew/src/main/java/com/example/demo/service/EmployService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employ;
+import com.example.demo.repo.EmployRepository;
+
+@Service
+public class EmployService {
+
+	@Autowired
+	private EmployRepository employRepository;
+	
+	public List<Employ> showDeptGenderWise(String dept, String gender) {
+		return employRepository.findByDeptAndGender(dept, gender);
+	}
+	
+	public List<Employ> showDeptWise(String dept) {
+		return employRepository.findByDept(dept);
+	}
+	
+	public String deleteEmploy(int empno) {
+		Employ employ = searchEmploy(empno);
+		employRepository.delete(employ);
+		return "Employ Record Deleted...";
+	}
+	
+	public String updateEmploy(Employ employ) {
+		employRepository.save(employ);
+		return "Employ Record Updated...";
+	}
+	
+	public String addEmploy(Employ employ) {
+		employRepository.save(employ);
+		return "Employ Record Inserted...";
+	}
+	
+	public List<Employ> showEmploy() {
+		return employRepository.findAll();
+	}
+	
+	public Employ searchEmploy(int empno) {
+		return employRepository.findById(empno).get();
+	}
+}
